17b94b241444b41d4d81057a64e91724ed61c9f3
ApricityJ <sophiejiang1029@gmail.com>
2023-10-29 10:09:04 +0800

    Initial commit

---
+++ constant.py
@@ -0,0 +1,22 @@
+import os
+
+dir_base = '/Users/sophie/workspace/data/contest'
+active_year = '2023'
+active_contest = '2'  # '1'
+active_phase = 'A'  # B
+
+dir_contest = os.path.join(dir_base, active_year, active_contest)
+dir_train = os.path.join(dir_contest, 'train')
+dir_test = os.path.join(dir_contest, active_phase)
+dir_result = os.path.join(dir_contest, 'result')
+dir_preprocess = os.path.join(dir_contest, 'preprocess')
+dir_model = os.path.join(dir_contest, 'model')
+
+file_name_train = 'train.p'
+file_name_test = 'test.p'
+
+random_states = [0, 1, 42, 2023, 29]
+active_random_state = 29
+
+LABEL = 'FLAG'
+ID = 'CUST_NO'
---
+++ utils/exporter.py
@@ -0,0 +1,36 @@
+from pandas import DataFrame
+
+from constant import *
+
+
+def to_csv(p: str, n: str, df: DataFrame):
+    df.to_csv(os.path.join(p, f'{n}.csv'), index=False)
+
+
+def export_ds(p: str, ds: dict):
+    for k, v in ds.items():
+        to_csv(p, k, v)
+
+
+def export_df_to_train(n: str, df: DataFrame):
+    to_csv(dir_train, n, df)
+
+
+def export_df_to_preprocess(n: str, df: DataFrame):
+    to_csv(dir_preprocess, n, df)
+
+
+def export_df_to_test(n: str, df: DataFrame):
+    to_csv(dir_test, n, df)
+
+
+def export_ds_to_preprocess(ds: dict):
+    export_ds(dir_preprocess, ds)
+
+
+def export_ds_to_result(ds: dict):
+    export_ds(dir_result, ds)
+
+
+def to_submission(df: DataFrame):
+    return df.to_csv(dir_result.joinpath('result.csv'), header=False)
